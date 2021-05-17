3ccc8cf62b76b548aac34234e00130b1c3356f81
gnsnach <72245938+gnsnach@users.noreply.github.com>
2021-05-17 14:16:29 +0800

    Delete Final/ganesh/17521_RentalCarSys/CarRentalSys directory

--- Final/ganesh/17521_RentalCarSys/CarRentalSys/src/main/java/crg/repo/RoleRepo.java
+++
@@ -1,9 +0,0 @@
-package crg.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import crg.model.Role;
-
-public interface RoleRepo extends JpaRepository<Role, Integer> {
-
-}
--- Final/ganesh/17521_RentalCarSys/CarRentalSys/src/main/java/crg/repo/VehicleRepo.java
+++
@@ -1,9 +0,0 @@
-package crg.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import crg.model.Vehicle;
-
-public interface VehicleRepo extends JpaRepository <Vehicle, Long>{
-
-}
